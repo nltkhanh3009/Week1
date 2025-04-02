bf0a7cf2d30acff9f5e65515554060712c759303
nltkhanh3009 <nltkhanh3009@gmial.com>
2025-04-02 07:17:29 +0700

    update

---
+++ Interf.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public interface IDongVat
+{
+    void TiengKeu();
+}
+
+public class Cho : IDongVat
+{
+    public void TiengKeu()
+    {
+        Console.WriteLine("Gau gau");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IDongVat cho = new Cho();
+
+        cho.TiengKeu();
+        Console.ReadLine();
+    }
+}
---
+++ LopTruuTuong.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+public abstract class DongVat
+{
+    public abstract void TiengKeu();
+
+    // Phương thức chung
+    public void Ngu()
+    {
+        Console.WriteLine("zzzzzzz");
+    }
+}
+
+// Kế thừa abstract
+class Cho : DongVat
+{
+    public override void TiengKeu()
+    {
+        Console.WriteLine("Gau gau");
+    }
+}
+
+// Kế thừa abstract
+public class Meo : DongVat
+{
+    public override void TiengKeu()
+    {
+        Console.WriteLine("Meo Meo");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Tạo biến
+        DongVat cho = new Cho();
+        DongVat meo = new Meo();
+
+        cho.TiengKeu();
+        cho.Ngu();
+        meo.TiengKeu();
+        meo.Ngu();
+
+        Console.ReadLine();
+    }
+}
