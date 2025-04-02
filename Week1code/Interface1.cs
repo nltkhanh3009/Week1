99acaf8e2446c1f31ccf162497174da6fc93a449
nltkhanh3009 <nltkhanh3009@gmial.com>
2025-04-02 07:18:21 +0700

    update2

---
+++ Week1code/Interface1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week1code
+{
+    public interface IDongVat
+    {
+        void TiengKeu();
+    }
+
+    public class Cho : IDongVat
+    {
+        public void TiengKeu()
+        {
+            Console.WriteLine("Gau gau");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDongVat cho = new Cho();
+
+            cho.TiengKeu();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Week1code/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int a = 3;
+        int b = 9;
+        int kq = a+b;
+        Console.WriteLine(kq);
+    }
+}
